40295b110f0f8d38d04e4739ed0e3330c62a98fb
yasirelbouazzati <yasir12bouazzati@gmail.com>
2024-03-17 18:33:34 +0100

    activity almost complete

--- src/hooks/usePersons.jsx
+++ src/hooks/usePersons.jsx
@@ -1,20 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export const usePersons = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ]);
+  const apiUrl = "http://localhost:3001";
+  const [persons, setPersons] = useState([]);
 
-    const handleChangePersonsValue = (newValue) => {
-        setPersons(newValue)
-    }
+  useEffect(() => {
+    axios.get(`${apiUrl}/persons`).then((response) => {
+      const persons = response.data;
+      setPersons(persons);
+    });
+  });
 
-    return {
-        persons,
-        handleChangePersonsValue
-    };
-}
+  const handleChangePersonsValue = (newValue) => {
+    setPersons(newValue);
+  };
 
+  return {
+    persons,
+    handleChangePersonsValue,
+  };
+};
+
+[
+  { name: "Arto Hellas", number: "040-123456", id: 1 },
+  { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+  { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+  { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+];
